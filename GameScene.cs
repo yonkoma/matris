786d3ae8d0adcde3137c927ee8f7faf38958a81c
Jake <jake@LordTetris.edu>
2018-07-10 06:42:19 -0400

    Fixed pause happening every frame F was held, now does it on press. Added dialogue for pause.

--- GameScene.cs
+++ GameScene.cs
@@ -6,21 +6,24 @@ public class GameScene : Node2D
     [Signal]
     delegate void Play_Pause();
     
-	public override void _Ready()
-	{
-		// Called every time the node is added to the scene.
-		// Initialization here
-	}
+    public override void _Ready()
+    {
+	// Called every time the node is added to the scene.
+	// Initialization here
+    }
+    
+    public override void _Process(float delta)
+    {
+	// Called every frame. Delta is time since last frame.
+	// Update game logic here.
+	
+	// Send out a signal on play / pause
+	
+    }
 
-	public override void _Process(float delta)
-	{
-		// Called every frame. Delta is time since last frame.
-		// Update game logic here.
-
-	    // Send out a signal on play / pause
-	    if (Input.IsActionPressed("play_pause"))
-	    {
-			EmitSignal(nameof(Play_Pause));
-	    }
-	}
+    public override void _Input(InputEvent x)
+    {
+	if(x.IsActionPressed("play_pause"))
+	    EmitSignal(nameof(Play_Pause));
+    }
 }
--- PlayPrompt.cs
+++ PlayPrompt.cs
@@ -4,24 +4,28 @@ using System;
 public class PlayPrompt : Label
 {
     // Member variables here
-	bool paused = true;
+    bool paused = true;
     public override void _Ready()
     {
-		GetNode("/root/RootNode").Connect("Play_Pause", this, nameof(On_Pause));
+	GetNode("/root/RootNode").Connect("Play_Pause", this, nameof(On_Pause));
         // Called every time the node is added to the scene.
         // Initialization here
         
     }
-	
-	public void On_Pause() 
-	{
-		this.Text = "";
-	}
-
-//    public override void _Process(float delta)
-//    {
-//        // Called every frame. Delta is time since last frame.
-//        // Update game logic here.
-//        
-//    }
+    
+    public void On_Pause() 
+    {
+	if(paused)
+	    this.Text = "";
+	else
+	    this.Text = "Paused! Press F to resume.";
+	paused = !paused;
+    }
+    
+    //    public override void _Process(float delta)
+    //    {
+    //        // Called every frame. Delta is time since last frame.
+    //        // Update game logic here.
+    //        
+    //    }
 }
