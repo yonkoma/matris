dc23349066e1f73335ca489d537461fbb44a72be
yonnn <mail@yanyon.xyz>
2018-07-10 01:19:38 -0700

    Merged yonktris
    commit 30a6c804ad2ab02032a8ae6c439cac21e99f0d8e
    Author: yonnn <mail@yanyon.xyz>
    Date:   Tue Jul 10 01:15:22 2018 -0700
    
        Continued coding, switchted to new branch

--- Tetromino.cs
+++ Tetromino.cs
@@ -1,54 +1,49 @@
 using Godot;
 using System;
+using static BagGenerator;
 
 public class Tetromino
 {
-	public enum Tile
+	public enum Rotation
 	{
-		Empty  = -1,
-		Red    = 0,
-		Green  = 1,
-		Blue   = 2,
-		Orange = 3,
-		Yellow = 4,
-		Cyan   = 5,
-		Purple = 6,
-		Gray   = 7,
-	};
+		Left  = -1,
+		Right = 1,
+	}
 
-	private static readonly Vector2[] Z_MINO = {new Vector2(-1, -1), new Vector2(0, -1), new Vector2(0, 0), new Vector2(1, 0)};
-	private static readonly Vector2[] S_MINO = {new Vector2(-1, 0), new Vector2(0, 0), new Vector2(0, -1), new Vector2(1, -1)};
-	private static readonly Vector2[] J_MINO = {new Vector2(-1, -1), new Vector2(-1, 0), new Vector2(0, 0), new Vector2(1, 0)};
-	private static readonly Vector2[] L_MINO = {new Vector2(-1, 0), new Vector2(0, 0), new Vector2(1, 0), new Vector2(1, -1)};
-	private static readonly Vector2[] O_MINO = {new Vector2(0, 0), new Vector2(0, -1), new Vector2(1, -1), new Vector2(1, 0)};
-	private static readonly Vector2[] I_MINO = {new Vector2(-1, 0), new Vector2(0, 0), new Vector2(1, 0), new Vector2(2, 0)};
-	private static readonly Vector2[] T_MINO = {new Vector2(0, 0), new Vector2(-1, 0), new Vector2(0, -1), new Vector2(1, 0)};
+	private static readonly Vector2[] Z_TETROMINO = {new Vector2(-1, -1), new Vector2(0, -1), new Vector2(0, 0), new Vector2(1, 0)};
+	private static readonly Vector2[] S_TETROMINO = {new Vector2(-1, 0), new Vector2(0, 0), new Vector2(0, -1), new Vector2(1, -1)};
+	private static readonly Vector2[] J_TETROMINO = {new Vector2(-1, -1), new Vector2(-1, 0), new Vector2(0, 0), new Vector2(1, 0)};
+	private static readonly Vector2[] L_TETROMINO = {new Vector2(-1, 0), new Vector2(0, 0), new Vector2(1, 0), new Vector2(1, -1)};
+	private static readonly Vector2[] O_TETROMINO = {new Vector2(0, 0), new Vector2(0, -1), new Vector2(1, -1), new Vector2(1, 0)};
+	private static readonly Vector2[] I_TETROMINO = {new Vector2(-1, 0), new Vector2(0, 0), new Vector2(1, 0), new Vector2(2, 0)};
+	private static readonly Vector2[] T_TETROMINO = {new Vector2(0, 0), new Vector2(-1, 0), new Vector2(0, -1), new Vector2(1, 0)};
 
 	private Vector2[] minoTiles;
+	
 
-	public Tetromino(Tile tileType) {
+	public Tetromino(Mino tileType) {
 		switch(tileType)
 		{
-			case Tile.Red:
-				minoTiles = (Vector2[])Z_MINO.Clone();
+			case Mino.Red:
+				minoTiles = (Vector2[])Z_TETROMINO.Clone();
 				break;
-			case Tile.Green:
-				minoTiles = (Vector2[])S_MINO.Clone();
+			case Mino.Green:
+				minoTiles = (Vector2[])S_TETROMINO.Clone();
 				break;
-			case Tile.Blue:
-				minoTiles = (Vector2[])J_MINO.Clone();
+			case Mino.Blue:
+				minoTiles = (Vector2[])J_TETROMINO.Clone();
 				break;
-			case Tile.Orange:
-				minoTiles = (Vector2[])L_MINO.Clone();
+			case Mino.Orange:
+				minoTiles = (Vector2[])L_TETROMINO.Clone();
 				break;
-			case Tile.Yellow:
-				minoTiles = (Vector2[])O_MINO.Clone();
+			case Mino.Yellow:
+				minoTiles = (Vector2[])O_TETROMINO.Clone();
 				break;
-			case Tile.Cyan:
-				minoTiles = (Vector2[])I_MINO.Clone();
+			case Mino.Cyan:
+				minoTiles = (Vector2[])I_TETROMINO.Clone();
 				break;
-			case Tile.Purple:
-				minoTiles = (Vector2[])T_MINO.Clone();
+			case Mino.Purple:
+				minoTiles = (Vector2[])T_TETROMINO.Clone();
 				break;
 			default:
 				throw new Exception("Tried to create tetromino of unknown type");
