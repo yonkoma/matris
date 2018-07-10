571c09d89a4df9985d642b2cf46d675ac530e8a2
yonnn <mail@yanyon.xyz>
2018-07-10 02:45:18 -0700

    Wrote main bag generator code

--- BagGenerator.cs
+++ BagGenerator.cs
@@ -1,6 +1,7 @@
 using Godot;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public class BagGenerator
 {
@@ -19,9 +20,34 @@ public class BagGenerator
 
 	public enum TetrominoTypes
 	{
-		
+		Z,
+		S,
+		J,
+		L,
+		O,
+		I,
+		T,
 	}
 
-	private static readonly List<Mino> STANDARD_BAG = new List<Mino>();
+	private static readonly List<TetrominoTypes> STANDARD_BAG = Enum.GetValues(typeof(TetrominoTypes)).Cast<TetrominoTypes>().ToList();
+
+	private List<TetrominoTypes> tetrominoBag = new List<TetrominoTypes>();
+	
+	public BagGenerator()
+	{
+		addNewBag();
+	}
+
+	private void addNewBag()
+	{
+		List<TetrominoTypes> newBag = new List<TetrominoTypes>(STANDARD_BAG);
+		Random rand = new Random();
+		for(int i = newBag.Count; i > 0; i--)
+		{
+			int chosenTetrominoIndex = rand.Next(i);
+			tetrominoBag.Add(newBag.ElementAt(chosenTetrominoIndex));
+			newBag.RemoveAt(chosenTetrominoIndex);
+		}
+	}
 
 }
