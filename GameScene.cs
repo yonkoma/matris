eeb05bb4efac84e54a888fa8f0b7ce6c205fb7e9
Jake <jake@LordTetris.edu>
2018-07-10 06:11:21 -0400

    Added new signal Play_Pause for when the user presses the play_pause button. Emitted by RootNode. Added dialogue that dissapears on pause (temporary)

--- GameScene.cs
+++ GameScene.cs
@@ -3,7 +3,9 @@ using System;
 
 public class GameScene : Node2D
 {
-
+    [Signal]
+    delegate void Play_Pause();
+    
 	public override void _Ready()
 	{
 		// Called every time the node is added to the scene.
@@ -14,5 +16,11 @@ public class GameScene : Node2D
 	{
 		// Called every frame. Delta is time since last frame.
 		// Update game logic here.
+
+	    // Send out a signal on play / pause
+	    if (Input.IsActionPressed("play_pause"))
+	    {
+			EmitSignal(nameof(Play_Pause));
+	    }
 	}
 }
---
+++ PlayPrompt.cs
@@ -0,0 +1,27 @@
+using Godot;
+using System;
+
+public class PlayPrompt : Label
+{
+    // Member variables here
+	bool paused = true;
+    public override void _Ready()
+    {
+		GetNode("/root/RootNode").Connect("Play_Pause", this, nameof(On_Pause));
+        // Called every time the node is added to the scene.
+        // Initialization here
+        
+    }
+	
+	public void On_Pause() 
+	{
+		this.Text = "";
+	}
+
+//    public override void _Process(float delta)
+//    {
+//        // Called every frame. Delta is time since last frame.
+//        // Update game logic here.
+//        
+//    }
+}
